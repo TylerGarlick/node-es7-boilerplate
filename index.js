63aaa2edb0cb862ae0165cfb9c60f90a46f806aa
Tyler Garlick <tjgarlick@gmail.com>
2017-02-07 01:05:05 -0700

    Fixes to boilerplate

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require('./dist');
+const ENV = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (ENV !== 'production') {
+  require('babel-register');
+  require('babel-polyfill');
+  module.exports = require('./src');
+} else {
+  module.exports = require('./dist');
+}
